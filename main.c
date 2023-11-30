4d9f270127dd677a04b5ce10511636ed7941ed17
livingod <144990631+livingod@users.noreply.github.com>
2023-11-30 15:28:28 +0900

    02

--- main.c
+++ main.c
@@ -1,27 +1,32 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <math.h>
 
-struct student{
-         int     ID;
-         char    name[100];
-         float  grade;
-  };
-  
+struct point{
+       int   x;
+       int   y;
+       };
+
 int main(int argc, char *argv[])
 {
-    //instance 선언
-    struct student s1 = {12345678,"Bboong",2.67}; 
+    struct point p1,p2;
+    int xdiff, ydiff;
+    double dist;
     
-    //다른 값 변경
-    s1.ID = 87654321;
-    s1.name[0] = 'K';
-    s1.name[1] = 'k';
-    s1.grade = 2.16;
+    printf("input p1 coordinate (x y): \n");
+    scanf("%d %d", &p1.x,&p1.y);
     
-    printf("ID: %d\n",s1.ID); 
-    printf("name: %s\n",s1.name);
-    printf("grade: %f\n",s1.grade);
- 
+    printf("input p2 coordinate (x y): \n");
+    scanf("%d %d", &p2.x,&p2.y);
+    
+    //distance 계산
+    xdiff = p2.x - p1.x;
+    ydiff = p2.y - p2.y;
+    dist = sqrt(xdiff*xdiff + ydiff*ydiff); 
+    
+    
+    printf("distance: %f\n",dist);
+   
   system("PAUSE");	
   return 0;
 }
